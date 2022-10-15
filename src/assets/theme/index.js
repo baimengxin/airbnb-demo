16d0fef76dd1a9da5298d217415f7b6f4f9b919e
小白萌新 <3036403996@qq.com>
2022-10-16 02:21:14 +0800

    feat: header组件左侧 logo 实现

---
+++ src/assets/theme/index.js
@@ -0,0 +1,8 @@
+const theme = {
+    color: {
+        primayColor: '#ff385c',
+        secondaryColor: '#00848A'
+    }
+}
+
+export default theme
--- src/components/app-header/c-cpns/header-left/style.js
+++ src/components/app-header/c-cpns/header-left/style.js
@@ -2,4 +2,13 @@ import styled from "styled-components";
 
 export const LeftWrapper = styled.div`
     flex: 1;
+    /* color: var(--primary-color); */
+    display: flex;
+    align-items: center;
+    color: ${props => props.theme.color.primayColor};
+
+    .logo {
+        margin-left: 24px;
+        cursor: pointer;
+    }
 `
