ef2c8250847755da78bd3adca4b923230c229430
小白萌新 <3036403996@qq.com>
2022-11-11 01:54:13 +0800

    feat: 首页-高评分数据的获取和展示

--- src/services/modules/home.js
+++ src/services/modules/home.js
@@ -1,7 +1,15 @@
 import hyRequest from "..";
 
+// 佛山高性价比房源
 export function getHomeGoodPriceData() {
     return hyRequest.get({
         url: '/home/goodprice'
+    })
+}
+
+// 佛山高分好评房源
+export function getHomeHighScoreData() {
+    return hyRequest.get({
+        url: '/home/highscore'
     })
 }
--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -1,29 +1,37 @@
-import { getHomeGoodPriceData } from '@/services'
+import { getHomeGoodPriceData, getHomeHighScoreData } from '@/services'
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 
-export const fetchHomeDataAction = createAsyncThunk('fetchdata', async () => {
-    const res = await getHomeGoodPriceData()
+export const fetchHomeDataAction = createAsyncThunk('fetchdata',  (payload, { dispatch }) => {
+    getHomeGoodPriceData().then(res => {
+        dispatch(changeGoodPriceInfoAction(res))
+    })
 
-    return res
+    getHomeHighScoreData().then(res => {
+        dispatch(changeHighScoreInfoAction(res))
+    })
 })
 
 const homeSlice = createSlice({
     name: 'home',
     initialState: {
-        goodPriceInfo: {}
+        goodPriceInfo: {},
+        highScoreInfo: {}
     },
     reducers: {
         changeGoodPriceInfoAction(state, { payload }) {
             state.goodPriceInfo = payload
-        }
+        },
+        changeHighScoreInfoAction(state, { payload }) {
+            state.highScoreInfo = payload
+          },
     },
-    extraReducers: {
-        [fetchHomeDataAction.fulfilled](state, {payload}) {
-            // console.log(payload);
-            state.goodPriceInfo = payload
-        }
-    }
+    // extraReducers: {
+    //     [fetchHomeDataAction.fulfilled](state, {payload}) {
+    //         // console.log(payload);
+    //         state.goodPriceInfo = payload
+    //     }
+    // }
 })
 
-export const { changeGoodPriceInfoAction } = homeSlice.actions
+export const { changeGoodPriceInfoAction, changeHighScoreInfoAction } = homeSlice.actions
 export default homeSlice.reducer
