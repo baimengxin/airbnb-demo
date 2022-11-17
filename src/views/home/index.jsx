0f0b33ceccf03ef13d602e987bf3445ed0167afa
小白萌新 <3036403996@qq.com>
2022-11-18 03:51:54 +0800

    refactor: 首页-v2区域的封装抽离

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -5,12 +5,7 @@ import { fetchHomeDataAction } from '@/store/modules/home'
 import HomeBanner from './c-cpns/home-banner'
 import { HomeWrapper } from './style'
 import HomeSectionV1 from './c-cpns/home-section-v1'
-import SectionHeader from '@/components/section-header'
-import SectionRooms from '@/components/section-rooms'
-import SectionTabs from '@/components/section-tabs'
-import { useCallback } from 'react'
-import { useState } from 'react'
-
+import HomeSectionV2 from './c-cpns/home-section-v2'
 
 const Home = memo(() => {
   // 从 redux 中获取数据
@@ -20,12 +15,7 @@ const Home = memo(() => {
     highScoreInfo: state.home.highScoreInfo
   }), shallowEqual)
 
-  // 处理数据
-  const [name, setName] = useState('佛山') 
-  const tabNames = disCountInfo.dest_address?.map(item => item.name)
-  const tabClickHandle = useCallback(function (index, name) {
-    setName(name)
-  }, [])
+  
 
   // 派发异步的事件：发送网络请求
   const dispatch = useDispatch()
@@ -39,12 +29,7 @@ const Home = memo(() => {
     <HomeWrapper>
       <HomeBanner />
       <div className="content">
-        <div className="discount">
-          <SectionHeader title={disCountInfo.title} subtitle={disCountInfo.subtitle} />
-          <SectionTabs tabNames={tabNames} tabClick={tabClickHandle} />
-          <SectionRooms roomList={disCountInfo.dest_list?.[name]} itemWidth={'33.33%'} />
-        </div>
-
+        <HomeSectionV2 infoData={ disCountInfo } />
         <HomeSectionV1 infoData={ goodPriceInfo } />
         <HomeSectionV1 infoData={ highScoreInfo } />
       </div>
