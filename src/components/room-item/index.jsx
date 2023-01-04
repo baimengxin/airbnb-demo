f87903e721b23b3b75b8f244b737cc5a55d1d86a
小白萌新 <3036403996@qq.com>
2023-01-04 23:06:05 +0800

    feat: 全部-RoomItem组件的轮播图实现效果

--- src/components/room-item/index.jsx
+++ src/components/room-item/index.jsx
@@ -1,11 +1,18 @@
 import PropTypes from 'prop-types'
-import React, { memo } from 'react'
-import { Rate } from 'antd';
-
+import React, { memo, useRef } from 'react'
+import { Rate, Carousel } from 'antd';
 import { ItemWrapper } from './style'
+import IconArrowLeft from '@/assets/svg/icon-arrow-left';
+import IconArrowRight from '@/assets/svg/icon-arrow-right';
 
 const RoomItem = memo((props) => {
     const { itemData, itemWidth='25%' } = props
+    const swiperRef = useRef()
+
+    // 左右图标切换
+    function controlClickHandle(isRight) {
+        isRight ? swiperRef.current.next() : swiperRef.current.prev()
+    }
 
   return (
     <ItemWrapper 
@@ -13,8 +20,32 @@ const RoomItem = memo((props) => {
         itemWidth={ itemWidth }
     >
         <div className="inner">
-            <div className="cover">
+            {/* <div className="cover">
                 <img src={itemData.picture_url} alt="" />
+            </div> */}
+            <div className="slider">
+                {/* 左右图标 */}
+                <div className="control">
+                    <div className="btn left" onClick={e => controlClickHandle(false)}>
+                        <IconArrowLeft width="24" height="24" />
+                    </div>
+                    <div className="btn right" onClick={e => controlClickHandle(true)}>
+                        <IconArrowRight width="24" height="24" />
+                    </div>
+                </div>
+
+                {/* 轮播图组件 */}
+                <Carousel dots={false} ref={swiperRef}>
+                    {
+                        itemData?.picture_urls?.map(item => {
+                            return (
+                                <div className="cover" key={item}>
+                                    <img src={item} alt="" />
+                                </div>
+                            )
+                        })
+                    }
+                </Carousel>
             </div>
 
             <div className="desc">
--- src/components/room-item/style.js
+++ src/components/room-item/style.js
@@ -10,6 +10,43 @@ export const ItemWrapper = styled.div`
         width: 100%;
     }
 
+    .slider {
+        position: relative;
+        cursor: pointer;
+
+        &:hover {
+            .control {
+                display: flex;
+            }
+        }
+
+        .control {
+            position: absolute;
+            z-index: 1;
+            left: 0;
+            right: 0;
+            top: 0;
+            display: none;
+            justify-content: space-between;
+            bottom: 0;
+            color: #fff;
+            /* background-color: skyblue; */
+
+            .btn {
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                width: 83px;
+                height: 100%;
+                background: linear-gradient(to left, transparent 0%, rgba(0, 0, 0, 0.25) 100%);
+
+                &.right {
+                    background: linear-gradient(to right, transparent 0%, rgba(0, 0, 0, 0.25) 100%);
+                }
+            }
+        }
+    }
+
     .cover {
         position: relative;
         box-sizing: border-box;
